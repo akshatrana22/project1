97076df9c6a773e48c2acc50c0647ccf0505d01a
akshatrana22 <akshatrana2707@gmail.com>
2018-05-18 12:08:25 +0530

    java projects

---
+++ AreaCalculator.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package area.calculator;
+
+/**
+ *
+ * @author HP User
+ */
+public class AreaCalculator {
+void sum()
+{
+     int i,j;
+        for(i=1;i<=3;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("x");
+        
+            }
+            
+        System.out.println();
+        
+        }
+    }
+    
+    public static void main(String[] args) {
+    AreaCalculator obj=new AreaCalculator();
+    obj.sum();
+    
+}
+}
+
+
+    
+
---
+++ AreaCalculator2.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package area.calculator;
+
+/**
+ *
+ * @author HP User
+ */
+public class AreaCalculator {
+
+    
+    public static void main(String[] args) {
+         int i,j;
+        for(i=1;i<=3;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("x");
+        
+            }
+            
+        System.out.println();
+        
+        }
+    }
+    
+}
+
+
+
+    
+
